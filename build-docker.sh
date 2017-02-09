fa4da9d88501900c77278327dc8caa05c6da7061
Habibie Faried <habibiefaried@gmail.com>
2017-02-09 10:26:16 +0700

    No exposed port

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 docker stop habibiefaried && docker rm habibiefaried
 docker build -t habibiefaried-image .
-docker run --name habibiefaried -h habibiefaried -p 80:80 -p 443:443 --restart=always -d habibiefaried-image
+docker run --name habibiefaried -h habibiefaried --restart=always -d habibiefaried-image
